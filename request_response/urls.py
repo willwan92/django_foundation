c740cde178f18702c609032078a328c856630d52
DESKTOP-SC2N0CF\admin <wilson3cy@163.com>
2024-07-10 16:44:35 +0800

    使用request.GET获取查询字符串参数

--- request_response/urls.py
+++ request_response/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # re_path(r"^weather/([a-z]+)/(\d{8})/$", views.weather),
     # 使用正则命名组提取url路径参数(关键字参数)，使用命名组时视图函数的形参必须和正则组名称一致
     re_path(r'^weather/(?P<city>\w+)/(?P<date>\d{8})/$', views.weather),
+    re_path(r'^get_query_params', views.get_query_params),
 ]
--- request_response/views.py
+++ request_response/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 def weather(request, city, date):
     print(city, date)
     return HttpResponse('weather')
+
+# GET /get_query_params?a=1&b=2&b=3
+def get_query_params(request):
+    query_dict = request.GET
+    # 获取单个值，返回字符串
+    a = query_dict.get('a')
+    # 获取某个参数的多个值，返回字符串列表
+    b = query_dict.getlist('b')
+    return HttpResponse('get_query_params: a=%s, b=[%s, %s]' % (a, b[0], b[1]))
