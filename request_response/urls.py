a15e8c9fd1d892b3ebc660dcfe5c335a3192471f
DESKTOP-SC2N0CF\admin <wilson3cy@163.com>
2024-07-10 17:04:08 +0800

    使用request.POST获取表单数据

--- request_response/urls.py
+++ request_response/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # 使用正则命名组提取url路径参数(关键字参数)，使用命名组时视图函数的形参必须和正则组名称一致
     re_path(r'^weather/(?P<city>\w+)/(?P<date>\d{8})/$', views.weather),
     re_path(r'^get_query_params', views.get_query_params),
+    re_path(r'^get_form_data', views.get_form_data),
 ]
--- request_response/views.py
+++ request_response/views.py
@@ -14,3 +14,12 @@ def get_query_params(request):
     # 获取某个参数的多个值，返回字符串列表
     b = query_dict.getlist('b')
     return HttpResponse('get_query_params: a=%s, b=[%s, %s]' % (a, b[0], b[1]))
+
+ # POST /get_form_data
+def get_form_data(request):
+    query_dict = request.POST
+    # 获取单个值，返回字符串
+    lang = query_dict.get('lang')
+    # 获取某个参数的多个值，返回字符串列表
+    framework = query_dict.getlist('framework')
+    return HttpResponse('get_query_params: lang=%s, framework=[%s, %s]' % (lang, framework[0], framework[1]))
